9d43a1929c11c857325063651c5c8d2881150493
djilali <derndjilali@gmail.com>
2025-05-29 18:39:39 +0100

    update file url

--- src/utils/MainAPI.tsx
+++ src/utils/MainAPI.tsx
@@ -1,2 +1,2 @@
 export const MainAPI = process.env.NEXT_PUBLIC_API_URL || "https://oggi-travel.com/api";
-export const FilesAPI = process.env.NEXT_PUBLIC_API_URL || "https://oggi-travel.com/storage";
+export const FilesAPI = process.env.NEXT_PUBLIC_API_URL || "https://oggi-travel.com/storage/app/public";
